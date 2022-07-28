8e9c2c56f0197c985de4fb609324960f2421ddb0
Ntumba Simon <simontumba@yahoo.fr>
2022-07-28 17:35:54 -0400

    un changement fait

--- ConsoleAppGitTutorial/ConsoleAppGitTutorial/Program.cs
+++ ConsoleAppGitTutorial/ConsoleAppGitTutorial/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleAppGitTutorial
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Merci mon Dieu!");
         }
     }
 }
