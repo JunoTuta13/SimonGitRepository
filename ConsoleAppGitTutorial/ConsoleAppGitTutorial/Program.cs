eb176504eb22cbf92f7ecccc4418d1e333de17d1
Ntumba Simon <simontumba@yahoo.fr>
2022-07-31 19:07:58 -0400

    pousser le dernier changement dans ma branche

--- ConsoleAppGitTutorial/ConsoleAppGitTutorial/Program.cs
+++ ConsoleAppGitTutorial/ConsoleAppGitTutorial/Program.cs
@@ -9,6 +9,7 @@ namespace ConsoleAppGitTutorial
             Console.WriteLine("Hello World!");
             Console.WriteLine("Merci mon Dieu!");
             Console.WriteLine("I make this change on my_branch. Hello Git!");
+            Console.WriteLine("Nouveau changement dans ma branche!");
         }
     }
 }
